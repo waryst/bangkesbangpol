35c911c434978a285fde6e2e11ce9de631385e09
waryst <w4ryst@gmail.com>
2023-07-29 21:15:51 +0700

    f

--- app/Http/Controllers/operator/SavesuaraController.php
+++ app/Http/Controllers/operator/SavesuaraController.php
@@ -30,15 +30,14 @@ class SavesuaraController extends Controller
             $jumlah_suara = $request->jumlah_suara;
             $suara_id=Suaracapres::where([['capres_id',$capres_id],['tps_id',$tps_id]])->first();
             $suara_id = $suara_id->id ?? '';
-            
-            Suaracapres::updateOrCreate(
-                ['id' => $suara_id],
-                [
-                    'capres_id' => $capres_id,
-                    'tps_id' => $tps_id,
-                    'jumlah' =>$jumlah_suara,
-                ]
-            );
+                Suaracapres::updateOrCreate(
+                    ['id' => $suara_id],
+                    [
+                        'capres_id' => $capres_id,
+                        'tps_id' => $tps_id,
+                        'jumlah' =>$jumlah_suara,
+                    ]
+                );
             $kirim['capres_id']=$capres_id;
             $kirim['jumlah_suara']=$jumlah_suara;
             return response()->json($kirim);    
@@ -122,7 +121,6 @@ class SavesuaraController extends Controller
             $jumlah_suara = $request->jumlah_suara;
             $suara_id=Suaracaleg::where([['caleg_id',$caleg_id],['tps_id',$tps_id]])->first();
             $suara_id = $suara_id->id ?? '';
-            if($jumlah_suara){
                 Suaracaleg::updateOrCreate(
                     ['id' => $suara_id],
                     [
@@ -130,8 +128,7 @@ class SavesuaraController extends Controller
                         'tps_id' => $tps_id,
                         'jumlah' =>$jumlah_suara,
                     ]
-                );            }
-
+                );      
             $kirim['caleg_id']=$caleg_id;
             $kirim['jumlah_suara']=$jumlah_suara;
             return response()->json($kirim);    
@@ -146,7 +143,6 @@ class SavesuaraController extends Controller
             $jumlah_suara = $request->jumlah_suara;
             $suara_id=Suaracalegprov::where([['caleg_id',$caleg_id],['tps_id',$tps_id]])->first();
             $suara_id = $suara_id->id ?? '';
-            if($jumlah_suara){
                 Suaracalegprov::updateOrCreate(
                     ['id' => $suara_id],
                     [
@@ -154,7 +150,7 @@ class SavesuaraController extends Controller
                         'tps_id' => $tps_id,
                         'jumlah' =>$jumlah_suara,
                     ]
-                );            }
+                );
 
             $kirim['caleg_id']=$caleg_id;
             $kirim['jumlah_suara']=$jumlah_suara;
@@ -170,7 +166,6 @@ class SavesuaraController extends Controller
             $jumlah_suara = $request->jumlah_suara;
             $suara_id=Suaracalegkab::where([['caleg_id',$caleg_id],['tps_id',$tps_id]])->first();
             $suara_id = $suara_id->id ?? '';
-            if($jumlah_suara){
                 Suaracalegkab::updateOrCreate(
                     ['id' => $suara_id],
                     [
@@ -178,8 +173,7 @@ class SavesuaraController extends Controller
                         'tps_id' => $tps_id,
                         'jumlah' =>$jumlah_suara,
                     ]
-                );            }
-
+                ); 
             $kirim['caleg_id']=$caleg_id;
             $kirim['jumlah_suara']=$jumlah_suara;
             return response()->json($kirim);    
@@ -191,71 +185,5 @@ class SavesuaraController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        dd();
-        // if($tipe=='capres'){
-        //     $capres_id = $request->capres_id;
-        //     $tps_id = $request->tps_id ;
-        //     $jumlah_suara = $request->jumlah_suara ?? 0;
 
-        //     $suara_id=Suaracapres::where([['capres_id',$capres_id],['tps_id',$tps_id]])->first();
-        //     $suara_id = $suara_id->id ?? '';
-            
-        //     Suaracapres::updateOrCreate(
-        //         ['id' => $suara_id],
-        //         [
-        //             'capres_id' => $capres_id,
-        //             'tps_id' => $tps_id,
-        //             'jumlah' =>$jumlah_suara,
-        //         ]
-        //     );
-        //     $kirim['capres_id']=$capres_id;
-        //     $kirim['jumlah_suara']=$jumlah_suara;
-        //     return response()->json($kirim);    
-        // }
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
