7e394d2a9b34be7bd7ea6832aa4537293765a75f
waryst <w4ryst@gmail.com>
2023-09-11 15:08:09 +0700

    .

---
+++ database/migrations/2023_09_11_144202_create_suaratidaksahs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suaratidaksahs', function (Blueprint $table) {
+            $table->uuid('tps_id');
+            $table->integer('presiden')->default(0);
+            $table->integer('gubernur')->default(0);
+            $table->integer('bupati')->default(0);
+            $table->integer('dpd')->default(0);
+            $table->integer('dpr_ri')->default(0);
+            $table->integer('dpr_prov')->default(0);
+            $table->integer('dpr_kab')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suaratidaksahs');
+    }
+};
--- resources/views/operator/conten/v_capres.blade.php
+++ resources/views/operator/conten/v_capres.blade.php
@@ -114,11 +114,20 @@
                     </div>
                 </div>
                 <div class="row justify-content-center">
-                    <div class="text-center mx-auto col-12 my-2" style="font-weight: 700;font-size: 2em">
+                    <div class="text-center mx-auto col-12 my-2 ">
                         @if ($pilih_tps->id ?? 0 != null)
-                            Nomer TPS :
-                            {{ sprintf('%02d', $pilih_tps->title) }}
+                            <span style="font-weight: 700;font-size: 2em">Nomer TPS :
+                                {{ sprintf('%02d', $pilih_tps->title) }}</span>
+                            <fieldset class="scheduler-border col-md-2">
+                                <legend class="scheduler-border" style="font-size: 12px">Suara Tidak Sah
+                                </legend>
+
+                                <input type="number" min="0" onKeyPress="if(this.value.length==3) return false;"
+                                    value="" class="text-center suara">
+                            </fieldset>
                         @endif
+
+
                     </div>
                     @foreach ($capres as $capres)
                         <div class="col-md-4">
@@ -149,7 +158,7 @@
                                                 <input type="number" min="0"
                                                     onKeyPress="if(this.value.length==3) return false;"
                                                     oninput="this.value = Math.abs(this.value)" autocomplete="off"
-                                                    id="{{ $capres->id }}" data-id="{{ $capres->id }}"
+                                                    id="{{ $capres->id }}"
                                                     data-default="{{ $capres->suaracapres[0]->jumlah ?? 0 }}"
                                                     data-desa="{{ $capres->id }}" onfocus="this.placeholder = ''"
                                                     onblur="this.placeholder = '0'"
@@ -195,17 +204,16 @@
             });
             $('.submit').keyup(function(e) {
                 data_default = $(this).data('default');
-                input_id = $(this).data('id');
+                input_id = $(this).attr('id');
                 tps_id = "{{ $pilih_tps->id ?? 0 }}";
                 jumlah_suara = $(this).val();
-                capres_id = $(this).data('id');
                 if (jumlah_suara != $("#" + input_id).attr("data-default")) {
                     document.getElementById(input_id).setAttribute("data-default", jumlah_suara);
-                    $('#save' + capres_id).html(`   `);
+                    $('#save' + input_id).html(`   `);
                     clearTimeout(Interval);
-                    Interval = setTimeout(saveSuara, 1000);
+                    Interval = setTimeout(saveSuara, 500);
                 } else {
-                    $('#save' + capres_id).html(`
+                    $('#save' + input_id).html(`
                             <svg viewBox="0 0 128 128" width="30" height="30">
                                 <path d="M0 64.37a9.67 9.67 0 0 1 2.94-4.67 8 8 0 0 1 9.2-.66 57.21 57.21 0 0 1 13.8 11 114.1 114.1 0 0 1 13.18 16.73c.17.26.36.5.56.77 1.83-3.43 3.54-6.85 5.44-10.17C56 58.23 70 41.83 88.16 29.21a125.64 125.64 0 0 1 28.44-14.62c5.76-2.12 11.08 1.82 11.22 6.91a1.32 1.32 0 0 0 .18.43v.24c-.11.49-.2 1-.32 1.47a7.91 7.91 0 0 1-5.35 5.95 105 105 0 0 0-25.56 13.15 125.27 125.27 0 0 0-33.1 34.91A138 138 0 0 0 48.5 108.5a7.69 7.69 0 0 1-6.15 5.27 4.66 4.66 0 0 0-.64.23h-1.44c-.1-.06-.19-.16-.3-.18a8.17 8.17 0 0 1-6.42-4.82 128.9 128.9 0 0 0-15.12-23.32c-3.76-4.53-7.75-8.87-12.87-11.88C2.92 72.25.87 70.46 0 67.48z" fill="#1148f1"></path>
                             </svg>          
@@ -214,23 +222,23 @@
             });
             $('.submit').blur(function() {
                 if (status == '') {
-                    input_id = $(this).data('id');
+                    input_id = $(this).attr('id');
                     tps_id = "{{ $pilih_tps->id ?? 0 }}";
                     data_default = $(this).data('default');
                     jumlah_suara = $(this).val();
-                    document.getElementById(input_id).setAttribute("data-default", jumlah_suara);
-                    var fd = new FormData();
-                    fd.append(input_id, jumlah_suara);
-                    fd.append('capres_id', input_id);
-                    fd.append('tps_id', tps_id);
-                    fd.append('jumlah_suara', jumlah_suara);
-
-                    $.ajaxSetup({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        }
-                    });
                     if (jumlah_suara != $("#" + input_id).attr("data-default")) {
+                        document.getElementById(input_id).setAttribute("data-default", jumlah_suara);
+                        var fd = new FormData();
+                        fd.append(input_id, jumlah_suara);
+                        fd.append('capres_id', input_id);
+                        fd.append('tps_id', tps_id);
+                        fd.append('jumlah_suara', jumlah_suara);
+
+                        $.ajaxSetup({
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            }
+                        });
 
                         $.ajax({
                             type: "POST",
