bc19d2aa9b459864c4c0c4564084176e53cd2e96
waryst <w4ryst@gmail.com>
2023-11-03 08:18:45 +0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if (env('APP_ENV') == 'production') {
+
+            URL::forceScheme('https');
+        }
     }
 }
